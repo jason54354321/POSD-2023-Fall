c8c39dda61ad54168fd55d7816e44c6534c71433
Jason <su3g4196ta8@gmail.com>
2023-09-20 00:10:29 +0800

    refactor: delete cmake's deps in project directory

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Dev/posd2023f_112598079_hw
-# Build directory: C:/Dev/posd2023f_112598079_hw/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(run_test "C:/Dev/posd2023f_112598079_hw/build/bin/run_test.exe")
-set_tests_properties(run_test PROPERTIES  _BACKTRACE_TRIPLES "C:/Dev/posd2023f_112598079_hw/CMakeLists.txt;41;add_test;C:/Dev/posd2023f_112598079_hw/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
