b9c5f1ba682ca143ff10e140b84b21b90abf9da4
Gustabo <su3g4196ta8@gmail.com>
2023-09-22 16:53:33 +0800

    [WIP]: try to use cli args

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
-#include <iostream>
 #include "./hello.h"
+#include <iostream>
 using namespace std;
 
-int main(void) {
+int main(int argc, char **argv) {
 
-  std::cout << sayHello("world") << '\n';
+  std::cout << sayHello("world") << '\n' << endl;
+  std::cout << argc << endl;
+  for (int i = 0; i < argc; i++) {
+    std::cout << argv[i] << endl;
+  }
 
   return 0;
 }
