4dd3cbc83ea1e53bc32df8e8e6fd997fb1757fe6
Jason <su3g4196ta8@gmail.com>
2023-10-01 12:47:15 +0800

    test(ut_file): path & name test with fixture testcase

--- test/ut_file.h
+++ test/ut_file.h
@@ -1,32 +1,32 @@
 #include "../src/file.h"
 #include <gtest/gtest.h>
 
-TEST(FileSuite, TestFilePath) {
-  File *file = new File("/test/123.jpg");
+class FileSuite : public testing::Test {
+protected:
+  File *_fileJPG;
+  File *_filePNG;
+  File *_fileNoExtension;
 
-  ASSERT_EQ(file->path(), "/test/123.jpg");
+  void SetUp() override {
+    _fileJPG = new File("/test/123.jpg");
+    _filePNG = new File("/test/456.png");
+    _fileNoExtension = new File("/test/789");
+  }
+
+  void TearDown() override {
+    delete (_fileJPG);
+    delete (_filePNG);
+  }
+};
+
+TEST_F(FileSuite, TestFilePath) {
+  ASSERT_EQ(_fileJPG->path(), "/test/123.jpg");
+  ASSERT_EQ(_filePNG->path(), "/test/456.png");
+  ASSERT_EQ(_fileNoExtension->path(), "/test/789");
 }
 
-TEST(FileSuite, TestFileName_456jpg) {
-  File *file = new File("/test/456.jpg");
-
-  ASSERT_EQ(file->name(), "456.jpg");
-}
-
-TEST(FileSuite, TestFileName_789gif) {
-  File *file = new File("/test/789.gif");
-
-  ASSERT_EQ(file->name(), "789.gif");
-}
-
-TEST(FileSuite, TestFileName_789noExtension) {
-  File *file = new File("/test/789");
-
-  ASSERT_EQ(file->name(), "789");
-}
-
-TEST(FileSuite, TestFileName_noRootBar) {
-  File *file = new File("789.gif");
-
-  ASSERT_EQ(file->name(), "789.gif");
+TEST_F(FileSuite, TestFileName) {
+  ASSERT_EQ(_fileJPG->name(), "123.jpg");
+  ASSERT_EQ(_filePNG->name(), "456.png");
+  ASSERT_EQ(_fileNoExtension->name(), "789");
 }
