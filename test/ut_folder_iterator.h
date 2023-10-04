ba4e7ee71805cf45571c344f9b5fdbea52c8ecfb
Jason <su3g4196ta8@gmail.com>
2023-10-04 13:24:39 +0800

    chore(ut_folder_iterator): put variable to fixture

--- test/ut_folder_iterator.h
+++ test/ut_folder_iterator.h
@@ -7,27 +7,37 @@ class FolderIteratorSuite : public ::testing::Test {
 protected:
   Folder *folderDocument_;
   Folder *folderMusic_;
+  File *music1_;
+  File *music2_;
+  Folder *musicSubFolder_;
+  File *musicSubFolderMusic1_;
 
   void SetUp() override {
     folderDocument_ = new Folder("documents/111");
+
     folderMusic_ = new Folder("music");
+    music1_ = new File("/music/123.mp3");
+    music2_ = new File("/music/456.mp3");
+    musicSubFolder_ = new Folder("/music/sub");
+    musicSubFolderMusic1_ = new File("/music/sub/999.mp3");
+
+    folderMusic_->add(music1_);
+    folderMusic_->add(music2_);
+    folderMusic_->add(musicSubFolder_);
+    musicSubFolder_->add(musicSubFolderMusic1_);
   }
 
   void TearDown() override {
     delete folderDocument_;
     delete folderMusic_;
+    delete music1_;
+    delete music2_;
+    delete musicSubFolder_;
+    delete musicSubFolderMusic1_;
   }
 };
 
 TEST_F(FolderIteratorSuite, TestFolderIterator) {
-  File *music1 = new File("/music/123.mp3");
-  File *music2 = new File("/music/456.mp3");
-  Folder *musicSubFolder = new Folder("/music/sub");
-
-  folderMusic_->add(music1);
-  folderMusic_->add(music2);
-  folderMusic_->add(musicSubFolder);
-
   Iterator *it = folderMusic_->createIterator();
 
   it->first();
@@ -43,74 +53,29 @@ TEST_F(FolderIteratorSuite, TestFolderIterator) {
 
   it->next();
   ASSERT_EQ(true, it->isDone());
-
-  delete music1;
-  delete music2;
-  delete musicSubFolder;
 }
 
 TEST_F(FolderIteratorSuite, TestFindCantFind) {
-  File *music1 = new File("/music/123.mp3");
-
-  Node *node;
-
-  node = music1->find("/music/000.mp3");
+  Node *node = music1_->find("/music/000.mp3");
   ASSERT_EQ(nullptr, node);
-
-  delete music1;
 }
 
 TEST_F(FolderIteratorSuite, TestFindFile) {
-  File *music1 = new File("/music/123.mp3");
-  File *music2 = new File("/music/456.mp3");
-
-  Node *node;
-
-  node = music1->find("/music/123.mp3");
+  Node *node = music1_->find("/music/123.mp3");
   ASSERT_EQ("123.mp3", node->name());
 
-  node = music2->find("/music/456.mp3");
+  node = music2_->find("/music/456.mp3");
   ASSERT_EQ("456.mp3", node->name());
-
-  delete music1;
-  delete music2;
 }
 
 TEST_F(FolderIteratorSuite, TestFindNestedFolder) {
-  Folder *musicSubFolder = new Folder("/music/sub");
-  File *musicSubFolderMusic1 = new File("/music/sub/999.mp3");
-
-  // top-level
-  folderMusic_->add(musicSubFolder);
-  // second-level
-  musicSubFolder->add(musicSubFolderMusic1);
-
   Node *node;
 
   node = folderMusic_->find("/music/sub/999.mp3");
   ASSERT_EQ("999.mp3", node->name());
-
-  delete musicSubFolder;
-  delete musicSubFolderMusic1;
 }
 
-// TODO: should be composite remove
-//			 had to start with find() method
 TEST_F(FolderIteratorSuite, TestRemove) {
-  File *music1 = new File("/music/123.mp3");
-  File *music2 = new File("/music/456.mp3");
-  Folder *musicSubFolder = new Folder("/music/sub");
-  File *musicSubFolderMusic1 = new File("/music/sub/999.mp3");
-
-  // top-level
-  folderMusic_->add(music1);
-  folderMusic_->add(music2);
-  folderMusic_->add(musicSubFolder);
-  // second-level
-  musicSubFolder->add(musicSubFolderMusic1);
-
-  ASSERT_EQ("999.mp3", folderMusic_->find("/music/sub/999.mp3")->name());
-
   // Remove
   folderMusic_->remove("/music/123.mp3");
   folderMusic_->remove("/music/sub/999.mp3");
@@ -119,9 +84,7 @@ TEST_F(FolderIteratorSuite, TestRemove) {
   ASSERT_EQ(nullptr, folderMusic_->find("/music/123.mp3"));
   ASSERT_EQ("456.mp3", folderMusic_->find("/music/456.mp3")->name());
   ASSERT_EQ(nullptr, folderMusic_->find("/music/sub/999.mp3"));
+}
 
-  delete music1;
-  delete music2;
-  delete musicSubFolder;
-  delete musicSubFolderMusic1;
+TEST_F(FolderIteratorSuite, TestGetChildByName) {
 }
