f62a934358902bc2df222287af3258aa8cdc578e
Gustabo <su3g4196ta8@gmail.com>
2023-10-18 00:48:22 +0800

    test

--- src/iterator.h
+++ src/iterator.h
@@ -12,11 +12,18 @@ class Folder;
 class Iterator {
 public:
   bool enable = true;
-  virtual ~Iterator(){};
-  virtual void first() = 0;
-  virtual Node *currentItem() const = 0;
-  virtual void next() = 0;
-  virtual bool isDone() const = 0;
+  virtual ~Iterator() {
+  }
+  virtual void first() {
+  }
+  virtual Node *currentItem() const {
+    return nullptr;
+  }
+  virtual void next() {
+  }
+  virtual bool isDone() const {
+    return true;
+  }
 };
 
 #endif // ITERATOR_H
