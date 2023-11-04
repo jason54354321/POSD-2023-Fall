c22a483950aa218587039f5551c4891e06797547
Jason <su3g4196ta8@gmail.com>
2023-11-05 03:00:54 +0800

    [WIP]: logic of visitFolder

--- src/tree_visitor.h
+++ src/tree_visitor.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "folder.h"
+#include "iterator.h"
 #include "order_by.h"
 #include "visitor.h"
 #include <string>
@@ -7,11 +9,26 @@
 using namespace std;
 
 class TreeVisitor : public Visitor {
+  private:
+    OrderBy _orderBy;
+    string _treeString;
+    int _currentLevel;
+
   public:
-    TreeVisitor(OrderBy orderBy);
+    TreeVisitor(OrderBy orderBy) : _orderBy(orderBy) {
+    }
 
     void visitFile(File *file);
-    void visitFolder(Folder *folder);
+    void visitFolder(Folder *folder) {
+        if (_orderBy == OrderBy::Name) {
+            Iterator *it = folder->createIterator(OrderBy::Name);
+            for (it->first(); !it->isDone(); it->isDone()) {
+                // print string according to `_currentLevel`
+
+                // if it's a folder interate again
+            }
+        }
+    }
 
     string getTree();
 };
