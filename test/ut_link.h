cf9a18a18b3fe66e56885b0d9d33bc39e3c1d9e6
Jason <su3g4196ta8@gmail.com>
2023-12-19 05:37:53 +0800

    test

--- test/ut_link.h
+++ test/ut_link.h
@@ -11,33 +11,33 @@
 class LinkTestSuite: public ::testing::Test {
 protected:
     virtual void SetUp() {
-        home = new Folder("structure3/home");
+        home = new Folder("structure/home");
 
-        profile = new File("structure3/home/my_profile");
+        profile = new File("structure/home/my_profile");
         home->add(profile);
 
-        link = new Link("structure3/home/mylink", profile);
+        link = new Link("structure/home/mylink", profile);
         home->add(link);
 
-        document = new Folder("structure3/home/Documents");
+        document = new Folder("structure/home/Documents");
         home->add(document);
 
-        favorite = new Folder("structure3/home/Documents/favorites");
+        favorite = new Folder("structure/home/Documents/favorites");
         document->add(favorite);
-        ddd = new File("structure3/home/Documents/favorites/domain-driven-design.pdf");
+        ddd = new File("structure/home/Documents/favorites/domain-driven-design.pdf");
         favorite->add(ddd);
-        ca = new File("structure3/home/Documents/favorites/clean-architecture.pdf");
+        ca = new File("structure/home/Documents/favorites/clean-architecture.pdf");
         favorite->add(ca);
-        cqrs = new File("structure3/home/Documents/favorites/cqrs.pdf");
+        cqrs = new File("structure/home/Documents/favorites/cqrs.pdf");
         favorite->add(cqrs);
 
-        note = new File("structure3/home/Documents/note.txt");
+        note = new File("structure/home/Documents/note.txt");
         document->add(note);
 
-        download = new Folder("structure3/home/Downloads");
+        download = new Folder("structure/home/Downloads");
         home->add(download);
 
-        funny = new File("structure3/home/Downloads/funny.png");
+        funny = new File("structure/home/Downloads/funny.png");
         download->add(funny);
 
     }
@@ -72,13 +72,13 @@ protected:
 
 TEST_F(LinkTestSuite, filelink_sanity) {
     ASSERT_EQ("mylink", link->name());
-    ASSERT_EQ("structure3/home/mylink", link->path());
+    ASSERT_EQ("structure/home/mylink", link->path());
 }
 
 TEST_F(LinkTestSuite, filelink_forward_action) {
     ASSERT_EQ(1 , link->numberOfFiles());
-    ASSERT_EQ("my_profile", link->find("structure3/home/my_profile")->name());
-    ASSERT_EQ("structure3/home/my_profile", link->findByName("my_profile").front());
+    ASSERT_EQ("my_profile", link->find("structure/home/my_profile")->name());
+    ASSERT_EQ("structure/home/my_profile", link->findByName("my_profile").front());
     ASSERT_THROW(link->add(ca), string);
     ASSERT_THROW(link->remove("123"), string);
     ASSERT_EQ(nullptr, link->getChildByName("123"));
